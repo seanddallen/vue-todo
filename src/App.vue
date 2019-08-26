e96e8e057b55c17dce652a9281ef4e14fb0cbc67
Sean Tayler <seantayler@gAM7acd39.local>
2019-08-25 18:02:15 -0700

    setup add/delete/edit todos

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app" class="container">
-    <img alt="Vue logo" src="./assets/logo.png" class="logo">
+    <img alt="Vue logo" src="./assets/logo.png" class="logo" />
+    <todo-list></todo-list>
   </div>
 </template>
 
@@ -11,17 +12,34 @@ import TodoList from './components/TodoList.vue'
 export default Vue.extend({
   name: 'app',
   components: {
+    TodoList
   },
 });
 </script>
 
-<style>
+
+
+<style lang="scss">
+
+* {
+  box-sizing: border-box;
+}
+.container {
+  max-width: 600px;
+  margin: 0 auto;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  font-size: 24px;
 }
+.logo {
+  display: block;
+  margin: 20px auto;
+  height: 75px;
+}
+
 </style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,5 +1,14 @@
 <template>
-
+    <div>
+        <input type="text" class="todo-input" placeholder="Enter New Todo" v-model="newTodo" @keyup.enter="addTodo" />
+        <div v-for="(todo, index) in todos" :key="todo.id" class="todo-item">
+            <div class="todo-item-left">
+                <div class="todo-item-label" v-if="!todo.editing" @dblclick="editTodo(todo)">{{ todo.title }}</div>
+                <input class="todo-item-edit" type="text" v-model="todo.title" v-else v-focus @blur="doneEdit(todo)" @keyup.enter="doneEdit(todo)" @keyup.esc="cancelEdit(todo)" />
+            </div>
+            <div class="remove-item" @click="removeTodo(index)">&times;</div>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
@@ -8,12 +17,143 @@ import Todo from './Todo.vue'
 
 export default Vue.extend({
   name: 'TodoList',
+  data: () => {
+      return {
+          newTodo: '', 
+          todoId: 2,
+          beforeEditCache: '',
+          todos: [
+              {
+                  id: 1,
+                  title: 'finish todo app',
+                  completed: false,
+                  editing: false
+              }
+            ]
+      }
+  },
+  methods: {
+      addTodo(){
+          if(this.newTodo.trim().length === 0) return
+          this.todos.push({
+              id: this.todoId,
+              title: this.newTodo,
+              completed: false,
+              editing: false
+          })
+          this.newTodo = ''
+          this.todoId++
+      },
+      removeTodo(index: number){
+          this.todos.splice(index, 1)
+      },
+      editTodo(todo: any){
+          this.beforeEditCache = todo.title
+          todo.editing = true
+      },
+      doneEdit(todo: any){
+          if(todo.title.trim().length === 0){
+              todo.title = this.beforeEditCache
+          }
+          todo.editing = false
+      },
+      cancelEdit(todo: any){
+          todo.title = this.beforeEditCache
+          todo.editing = true
+      }
+  },
+  directives: {
+      focus: {
+          inserted: (el) => {
+              el.focus()
+          }
+      }
+  },
   props: {
-    msg: String,
   },
 });
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+@import url("https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css");
+.todo-input {
+  width: 100%;
+  padding: 10px 18px;
+  font-size: 18px;
+  margin-bottom: 16px;
+  &:focus {
+    outline: 0;
+  }
+}
+.todo-item {
+  margin-bottom: 12px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  animation-duration: 0.3s;
+}
+.remove-item {
+  cursor: pointer;
+  margin-left: 14px;
+  &:hover {
+    color: black;
+  }
+}
+.todo-item-left {
+  display: flex;
+  align-items: center;
+}
+.todo-item-label {
+  padding: 10px;
+  border: 1px solid white;
+  margin-left: 12px;
+}
+.todo-item-edit {
+  font-size: 24px;
+  color: #2c3e50;
+  margin-left: 12px;
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ccc; //override defaults
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  &:focus {
+    outline: none;
+  }
+}
+.completed {
+  text-decoration: line-through;
+  color: grey;
+}
+.extra-container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 16px;
+  border-top: 1px solid lightgrey;
+  padding-top: 14px;
+  margin-bottom: 14px;
+}
+button {
+  font-size: 14px;
+  background-color: white;
+  appearance: none;
+  &:hover {
+    background: lightgreen;
+  }
+  &:focus {
+    outline: none;
+  }
+}
+.active {
+  background: lightgreen;
+}
+// CSS Transitions
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.2s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
 </style>
